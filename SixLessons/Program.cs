148cfcdfdf70c22ce2079b2920197a0c8b2b2409
Fetis_ep98@top-academy.ru <Fetis_ep98@top-academy.ru>
2026-01-30 20:46:21 +0300

    дз 6

---
+++ SixLessons/Program.cs
@@ -0,0 +1,81 @@
+﻿using static System.Console;
+public class Account
+{
+    private decimal _balance;
+    private readonly object _lock = new object(); // неизменяемая ссылка на объект
+
+    public decimal Balance
+    {
+        get
+        {
+            lock (_lock)
+            { 
+                return _balance;
+            }
+        }
+    }
+
+    public Account(decimal iBalance)
+    {
+        _balance = iBalance;
+    }
+
+    public void Credit(decimal amount)
+    {
+        lock (_lock)
+        {
+            _balance += amount;
+            WriteLine($"Пополнили: + {amount}, Баланс: {_balance}");
+        }
+    }
+
+    public bool Debit(decimal amount)
+    {
+        lock (_lock)
+        {
+            if (_balance >= amount)
+            {
+                _balance -= amount;
+                WriteLine($"Списали: -{amount}, Баланс: {_balance}");
+                return true;
+            }
+            else
+            {
+                WriteLine($"ERROR: Нельзя снять {amount}, есть только {_balance}");
+                return false;
+            }
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        var account = new Account(1000);
+        account.Credit(1000);
+        
+        for (int i = 0; i < 4; i++)
+        {
+            int threadId = i;
+            Thread thread = new Thread(() =>
+            {
+                WriteLine($"Поток {threadId}");
+
+
+                if (account.Debit(1000))
+                {
+                    WriteLine($"Поток {threadId} успешно снял деньги");
+                }
+                else
+                {
+                    WriteLine($"Поток {threadId} не смог снять деньги");
+                }
+
+            });
+            thread.Start();
+        }
+
+        Thread.Sleep(100);
+        WriteLine($"\nИтоговый баланс: {account.Balance}");
+    }
+}
